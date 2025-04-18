58ce0a65fd0a3923a8f5fa00e55f2c14c1580f25
RhauanS <santosrhauan@gmail.com>
2025-04-18 18:41:01 -0300

    criação de um navbar no home

--- Peguntas_e_respostas/php_files/home.php
+++ Peguntas_e_respostas/php_files/home.php
@@ -6,7 +6,58 @@ require_once 'protege.php';
 
 ?>
 
-<form action="logout.php" method="post">
-    <button type="submit">Sair</button>
+<!DOCTYPE html>
+<html lang="en">
 
-</form>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+        <div class="container">
+
+            <button class="navbar-toggler" data-toggle="collapse" data-target="#Nav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="Nav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Início</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Sobre</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contato</a>
+                    </li>
+                </ul>
+            </div>
+
+            <form action="logout.php" method="post" class="ml-auto">
+                <button type="submit" >Sair</button>
+            </form>
+        </div>
+    </nav>
+
+
+
+
+
+
+
+
+    <!-- jQuery, Popper.js e Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
